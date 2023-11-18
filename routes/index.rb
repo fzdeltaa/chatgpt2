36f20eb55ee45fe508e9d31110b2a35edfc3c9b0
Faza Denandra <112379648+fzdeltaa@users.noreply.github.com>
2023-11-18 19:11:23 +0700

    super

--- routes/index.rb
+++ routes/index.rb
@@ -18,7 +18,7 @@ get '/messages/?:userid?' do
                   .where.not('users.userid = ?', session[:userid])
   @users.each do |user|
     receiver_username = User.find(user.receiverid).username
-    user.content = decrypt_aes(user.content, receiver_username)
+    user.content = encrypt_reverse(decrypt_aes(user.content, receiver_username))
   end
 
   @displayname = User.find(session[:userid]).displayname
@@ -36,6 +36,8 @@ get '/messages/?:userid?' do
         receiver_id = Message.find(message.messageid).receiverid
         receiver_username = User.find(receiver_id).username
         message.content = decrypt_aes(message.content, receiver_username)
+
+        message.content = encrypt_reverse(message.content) if message.dobel == 1
       end
 
       @show_right_partial = true
@@ -49,15 +51,16 @@ end
 
 post '/messages/:userid' do
   require_login
-  content = params['content']
+  dobel = 1
+  dobel = 0 if params['reverse'] == 'benar'
 
-  content = encrypt_reverse(content) if params['reverse'] == 'benar'
+  content = encrypt_reverse(params['content'])
 
   username = User.find(params['receiverid']).username
   content = encrypt_aes(content, username)
   if params['file']
     time = Time.new
-    filename = "#{time.strftime("%s")}#{rand(1..100)}.png"
+    filename = "#{time.strftime('%s')}#{rand(1..100)}.png"
     image_path = "/img/#{filename}"
 
     if params['reverse'] == 'benar'
@@ -69,7 +72,7 @@ post '/messages/:userid' do
     end
   end
 
-  Message.create(senderid: session[:userid], receiverid: params['receiverid'], content: content, image_url: image_path)
+  Message.create(senderid: session[:userid], receiverid: params['receiverid'], content: content, image_url: image_path, dobel: dobel)
   params.delete('file')
   redirect "/messages/#{params['receiverid']}"
 end
