17021009055c0e679e0f912b5c3b0bc213287eba
Faza Denandra <112379648+fzdeltaa@users.noreply.github.com>
2023-11-18 19:13:31 +0700

    Update index.rb

--- routes/index.rb
+++ routes/index.rb
@@ -21,7 +21,7 @@ get '/messages/?:userid?' do
                   .where.not('users.userid = ?', session[:userid])
   @users.each do |user|
     receiver_username = User.find(user.receiverid).username
-    user.content = decrypt_aes(user.content, receiver_username)
+    user.content = encrypt_reverse(decrypt_aes(user.content, receiver_username))
   end
 
   @displayname = User.find(session[:userid]).displayname
@@ -39,6 +39,8 @@ get '/messages/?:userid?' do
         receiver_id = Message.find(message.messageid).receiverid
         receiver_username = User.find(receiver_id).username
         message.content = decrypt_aes(message.content, receiver_username)
+        message.content = encrypt_reverse(message.content) if message.dobel == 1
+      
         filename = bucket.file message.image_url
         message.image_url = filename.signed_url(expires: 3600)
       end
